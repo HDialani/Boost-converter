6ed13da6e92802d0527e8cc63f9e3e66c7731e22
HDialani <hiteshdialani@hotmail.com>
2022-04-21 13:53:11 +0200

    Please work

---
+++ Basics_of_boost_converter/Code/main.py
@@ -0,0 +1,8 @@
+# Author: Hitesh Dialani
+# Last modified: 21-04-22
+
+Switching_Frequency = float(8.0e04)   # Hz
+Output_Voltage = int(150)   # V
+Input_Voltage = int(45)   # V
+Load_Resistor = int(400)    # Ohm
+Current_Ripple_Ratio = float(0.4)
