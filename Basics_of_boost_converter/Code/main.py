ad86e1e1416c93b46dcfa4c2ec776f9315db2054
HDialani <hiteshdialani@hotmail.com>
2022-04-24 22:07:27 +0200

    CCM code for plots made

--- Basics_of_boost_converter/Code/main.py
+++ Basics_of_boost_converter/Code/main.py
@@ -1,5 +1,5 @@
 # Author: Hitesh Dialani
-# Last modified: 23-04-22
+# Last modified: 24-04-22
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -13,24 +13,129 @@ Delta_I_Ratio = float(0.4)
 ### Continous Conduction Mode
 D=1-V_in/V_out
 I_out=V_out/Load
+I_in=V_out*I_out/V_in
+I_ripple=Delta_I_Ratio*I_in
+Vd=0.6 #V
 
-N=1000 # Sampling resolution
+S=1000 # Sampling resolution
+N=4 #Number of signals, needs to be an even number
 Ts=1/F_s #s
-X= np.arange(0, 2*Ts,2*Ts/N)
+X= np.arange(0, N*Ts,N*Ts/S)
 
-V_L1=np.array(np.ones(len(np.arange(0,D*Ts,2*Ts/N)))* V_in)
-V_L2=np.array(np.ones(len(np.arange(Ts/N,(1-D)*Ts,2*Ts/N)))*(V_in-V_out))
-V_L3=np.array(np.ones(len(np.arange(Ts/N,D*Ts,2*Ts/N)))* V_in)
-V_L4=np.array(np.ones(len(np.arange(Ts/N,(1-D)*Ts,2*Ts/N)))*(V_in-V_out))
+V_sw=[]
+for i in range(N):
+    Intermediate1=np.array(np.ones(len(np.arange(0,D*Ts,N*Ts/S)))* 0)
+    V_sw.extend(Intermediate1)
+    Intermediate2=np.array(np.ones(len(np.arange(Ts/S,(1-D)*Ts,N*Ts/S)))*(V_out+Vd))
+    V_sw.extend(Intermediate2)
 
-V_La=np.append(V_L1,V_L2)
-V_Lb=np.append(V_L3,V_L4)
-V_L=np.append(V_La,V_Lb)
+I_sw=[]
+for i in range(N):
+    I_intermediate1=np.array(I_in-I_ripple+(np.arange(0,D*Ts,N*Ts/S))* 2*I_ripple/(D*Ts))
+    I_sw.extend(I_intermediate1)
+    I_intermediate2=np.array(np.ones(len(np.arange(Ts/S,(1-D)*Ts,N*Ts/S)))*0)
+    I_sw.extend(I_intermediate2)
 
-plt.subplot(2, 1, 1)
-plt.plot(1e6*X, V_L)
-plt.xlim([0,2*1e6*Ts])
-plt.ylim([V_in-V_out-5,V_in+5])
+V_d=[]
+for i in range(N):
+    Intermediate1=np.array(np.ones(len(np.arange(0,D*Ts,N*Ts/S)))*-V_out)
+    V_d.extend(Intermediate1)
+    Intermediate2=np.array(np.ones(len(np.arange(Ts/S,(1-D)*Ts,N*Ts/S)))*(Vd))
+    V_d.extend(Intermediate2)
+
+I_d=[]
+for i in range(N):
+    I_intermediate1=np.array(np.arange(0,D*Ts,N*Ts/S)* 0)
+    I_d.extend(I_intermediate1)
+    I_intermediate2=np.array(I_in+I_ripple-(np.arange(Ts/S,(1-D)*Ts,N*Ts/S))* 2*I_ripple/((1-D)*Ts))
+    I_d.extend(I_intermediate2)
+
+
+V_l=[]
+for i in range(N):
+    Intermediate1=np.array(np.ones(len(np.arange(0,D*Ts,N*Ts/S)))* V_in)
+    V_l.extend(Intermediate1)
+    Intermediate2=np.array(np.ones(len(np.arange(Ts/S,(1-D)*Ts,N*Ts/S)))*(V_in-V_out))
+    V_l.extend(Intermediate2)
+
+I_l=np. add(I_d,I_sw)
+
+V_c=[]
+for i in range(N):
+    Intermediate1=np.array(np.ones(len(np.arange(0,D*Ts,N*Ts/S)))*V_out)
+    V_c.extend(Intermediate1)
+    Intermediate2=np.array(np.ones(len(np.arange(Ts/S,(1-D)*Ts,N*Ts/S)))*V_out)
+    V_c.extend(Intermediate2)
+
+I_c=[]
+for i in range(N):
+    I_intermediate1=np.array(-I_out-np.arange(0,D*Ts,N*Ts/S) )
+    I_c.extend(I_intermediate1)
+    I_intermediate2=np.array(-I_out+I_in+I_ripple-(np.arange(Ts/S,(1-D)*Ts,N*Ts/S))* 2*I_ripple/((1-D)*Ts))
+    I_c.extend(I_intermediate2)
+
+plt.figure(1)
+plt.subplot(211)
+plt.plot(1e6*X, V_sw)
+plt.xlim([0,N*1e6*Ts])
+plt.ylim([-5,V_out+5])
+# plt.xlabel('Time [$\mu s$]')
+plt.ylabel('$V_{sw}$ [$V$]')
+
+plt.subplot(212)
+plt.plot(1e6*X, I_sw)
+plt.xlim([0,N*1e6*Ts])
+plt.ylim([-0.1,(I_in+I_ripple)+0.1])
 plt.xlabel('Time [$\mu s$]')
+plt.ylabel('$I_{sw}$ [$A$]')
+plt.suptitle('Transistor')
+
+plt.figure(2)
+plt.subplot(211)
+plt.plot(1e6*X, V_d)
+plt.xlim([0,N*1e6*Ts])
+plt.ylim([-V_out+-5,5])
+# plt.xlabel('Time [$\mu s$]')
+plt.ylabel('$V_{d}$ [$V$]')
+
+plt.subplot(212)
+plt.plot(1e6*X, I_d)
+plt.xlim([0,N*1e6*Ts])
+plt.ylim([-0.1,(I_in+I_ripple)+0.1])
+plt.xlabel('Time [$\mu s$]')
+plt.ylabel('$I_{d}$ [$A$]')
+plt.suptitle('Diode')
+
+plt.figure(3)
+plt.subplot(211)
+plt.plot(1e6*X, V_l)
+plt.xlim([0,N*1e6*Ts])
+plt.ylim([V_in-V_out-5,V_in+5])
+#plt.xlabel('Time [$\mu s$]')
 plt.ylabel('$V_L$ [$V$]')
+
+plt.subplot(212)
+plt.plot(1e6*X, I_l)
+plt.xlim([0,N*1e6*Ts])
+plt.ylim([-0.1,(I_in+I_ripple)+0.1])
+plt.xlabel('Time [$\mu s$]')
+plt.ylabel('$I_{L}$ [$A$]')
+plt.suptitle('Inductor')
+
+
+plt.figure(4)
+plt.subplot(211)
+plt.plot(1e6*X, V_c)
+plt.xlim([0,N*1e6*Ts])
+plt.ylim([0,V_out+5])
+#plt.xlabel('Time [$\mu s$]')
+plt.ylabel('$V_c$ [$V$]')
+
+plt.subplot(212)
+plt.plot(1e6*X, I_c)
+plt.xlim([0,N*1e6*Ts])
+plt.ylim([-I_out-0.1,(I_in+I_ripple-I_out)+0.1])
+plt.xlabel('Time [$\mu s$]')
+plt.ylabel('$I_{c}$ [$A$]')
+plt.suptitle('Capacitor')
 plt.show()
