fc898ce590b5f3235010068c8491b290f4e1274f
“Hitesh <hiteshdialani@hotmail.com>
2022-09-07 23:42:43 +0200

    Some text for control implementation

--- Controls/Simulink/Fake_Boost/Fake_boost_parameters.m
+++ Controls/Simulink/Fake_Boost/Fake_boost_parameters.m
@@ -1,19 +1,30 @@
 clc; clear; close all;
 
-V_DC=45;
+%% General Parameters
+V_DC=45; % 
+R_Load=400; 
+C_out=25e-3;
+V_f=1.5; 
 
+%% MOSFET 
 V_MOSFET_out=150;
-V_f=1.5;
+L_MOSFET=750e-6;
+D_MOSFET=(V_MOSFET_out+V_f-V_DC)/(V_MOSFET_out+V_f)*100; % Calculated duty cycle
+t_sw_MOSFET=1/(80e3);
+R_DC_MOSFET=0.3;
 
-D_MOSFET=(V_MOSFET_out+V_f-V_DC)/(V_MOSFET_out+V_f)*100;
+%% IGBT
+V_IGBT_out=300; 
+L_IGBT=1e-3;
+D_IGBT=(V_IGBT_out+V_f-V_DC)/(V_IGBT_out+V_f)*100; % Calculated duty cycle
+t_sw_IGBT=1/(15e3);
+R_DC_IGBT=0.1;
+
+%% Closed loop
 
 % % Works with continous powergui
- Kp=0.6;
- Ki=4.5;
-
-V_IGBT_out=300; 
-D_IGBT=(V_IGBT_out+V_f-V_DC)/(V_IGBT_out+V_f)*100;
-
-
+Kp=0.6;
+Ki=4.5;
+ 
 Kp1=0.8;
 Ki1=5;
