6db15f0fbf8660dfc124194ccdd6825a28377742
“Hitesh <hiteshdialani@hotmail.com>
2022-09-10 00:23:27 +0200

    Some more discription for Implementation notebook

--- Controls/Simulink/Controller/Openloop_voltage_control_parameters.m
+++ Controls/Simulink/Controller/Openloop_voltage_control_parameters.m
@@ -1,6 +1,12 @@
+% @Author: Hitesh Dialani
+% @Date: 09-09-2022
 clc; clear; close all;
 
-% User input
+%% Fixed parameters
+V_in=45;
+V_diode_forward_drop=1.5;
+
+%% User input
 V_MOSFET_out=150;
 V_IGBT_out=300;
 
@@ -10,22 +16,18 @@ f_sw_IGBT=15e3;
 V_MOSFET_overvolt_safety_factor=2;
 V_IGBT_overvolt_safety_factor=2;
 
-% Fixed parameters
-V_in=45;
-V_diode_forward_drop=1.5;
-
-% Calculated values 
+%% Calculated values 
 V_MOSFET_overvolt_protection=V_MOSFET_overvolt_safety_factor*V_MOSFET_out;
 V_IGBT_overvolt_protection=V_IGBT_overvolt_safety_factor*V_IGBT_out;
 
 D_MOSFET_CCM=(V_MOSFET_out+V_diode_forward_drop-V_in)/(V_MOSFET_out+V_diode_forward_drop);
 D_IGBT_CCM=(V_IGBT_out+V_diode_forward_drop-V_in)/(V_IGBT_out+V_diode_forward_drop);
 
-% Timer stuff
+%% Timer stuff
 Timer_period_MOSFET=round(60e6/f_sw_MOSFET);
 Timer_period_IGBT=round(60e6/f_sw_IGBT);
 
-% PI controllers
+%% PI controllers
 
 Kp_IGBT=0;
 Ki_IGBT=0;
